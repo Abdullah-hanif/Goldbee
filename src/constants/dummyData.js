e09128b6a7a00fdc9996788a160a3b8851b80b50
akifAbdullah <hinjorrcomp@gmail.com>
2022-11-17 23:54:31 +0500

    inboxScreen and ProfileScreen Done

--- src/constants/dummyData.js
+++ src/constants/dummyData.js
@@ -20,3 +20,38 @@ export const Data = [
     Price: '$ 437.00',
   },
 ];
+
+export const InboxPeople = [
+  {
+    id: 1,
+    name: 'John Michel',
+    productName: 'Beaded NecKlace',
+    message: 'Hello',
+    time: '12:36',
+    imageUri: require('../assets/SamplePictures/1.png'),
+  },
+  {
+    id: 2,
+    name: 'Sophia Martian',
+    productName: 'Pearl Ring',
+    message: 'Final Price Plese',
+    time: '10:24',
+    imageUri: require('../assets/SamplePictures/2.png'),
+  },
+  {
+    id: 3,
+    name: 'Natalia John',
+    productName: 'Wedding Ring',
+    message: 'what is the exact size',
+    time: 'Yeasterday',
+    imageUri: require('../assets/SamplePictures/3.png'),
+  },
+  {
+    id: 4,
+    name: 'Micheal Jordan',
+    productName: 'Earing Bracelat',
+    message: 'Full and Final Demand plz',
+    time: '2 days ago',
+    imageUri: require('../assets/SamplePictures/4.png'),
+  },
+];
