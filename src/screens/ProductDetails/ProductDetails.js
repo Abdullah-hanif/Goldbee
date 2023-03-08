d1949df7c7d24d95a055acc456260f343b957068
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2023-03-08 23:33:46 +0500

    Updated

--- src/screens/ProductDetails/ProductDetails.js
+++ src/screens/ProductDetails/ProductDetails.js
@@ -171,7 +171,7 @@ const ProductDetails = ({ navigation, route }) => {
             style={{ height: 50, width: 50, borderRadius: 100 }}
             source={
               sellerDetail?.profile_picture == 'http://95.179.209.186/'
-                ? require('../../assets/Icons/noPic.png.png')
+                ? require('../../assets/Icons/noPic.png')
                 : { uri: sellerDetail?.profile_picture }
             }
           />
